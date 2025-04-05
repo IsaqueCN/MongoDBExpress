9105ba1d15c7370bd941fa54b1506f9f8cd6ad6d
IsaqueCN <ggomesnascimento123@gmail.com>
2025-04-04 21:18:53 -0300

    Adicionando rotas put e delete para usuario

--- MongoCollections/Usuario/UsuarioRouter.js
+++ MongoCollections/Usuario/UsuarioRouter.js
@@ -4,16 +4,47 @@ const mongoose = require("mongoose");
 
 let router = Express.Router();
 
+async function catchErr(req, res, err) {
+    res.status(400).json({error: err.message});
+}
 router.get("/", async (req, res) => {
     let result = await Usuario.find();
     res.status(200).json(result);
 })
 
 router.post("/", async (req, res) => {
-    console.log(req.body)
+    try {
     let result = await Usuario.create(req.body);
 
     res.status(200).json(result);
+    } catch (err) {
+        catchErr(req, res, err);
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+    let id = req.params.id;
+    let result = await Usuario.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+
+    res.status(200).json(result);
+    } catch (err) {
+        catchErr(req, res, err)
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        let id = req.params.id;
+        let result = await Usuario.findByIdAndDelete(id);
+        
+        if (result == null)
+            res.status(404).json({error: "Usuario not found"});
+        else
+            res.status(200).json(result);
+    } catch(err) {
+        catchErr(req, res, err);
+    }
 })
 
 module.exports = router;
